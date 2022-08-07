5516f767d24d0a853b7cf44cd60ff36fa1b117b3
Olli Ulmala <ulmala.olli@hotmail.com>
2022-08-07 14:00:49 +0300

    add entity for sensor reading

---
+++ src/entities/sensor_reading.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class SensorReading:
+    data_format: int
+    humidity: float
+    temperature: float
+    pressure: float
+    acceleration: float
+    acceleration_x: int
+    acceleration_y: int
+    acceleration_z: int
+    tx_power: int
+    battery: int
+    movement_counter: int
+    measurement_sequence_number: int
+    mac: str
