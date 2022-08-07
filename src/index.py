0324f9c2a44e54d519cdd3b531768e797dcbc450
Olli Ulmala <ulmala.olli@hotmail.com>
2022-08-07 20:39:26 +0300

    fix timing

--- src/index.py
+++ src/index.py
@@ -1,5 +1,6 @@
 import datetime
 import json
+import time
 from entities.sensor_reading import SensorReading
 from repositories.sensor_reading_repository import sensor_reading_repository
 from ruuvitag_sensor.ruuvi import RuuviTagSensor
@@ -13,13 +14,15 @@ TIMEOUT = 5
 
 def main():
     while True:
+        current_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         data = RuuviTagSensor.get_data_for_sensors(MACS, TIMEOUT)
         for _, v in data.items():
-            v['datetime'] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            v['datetime'] = current_time
             reading = json.loads(
                 json.dumps(v), object_hook=lambda d: SensorReading(**d)
             )
             sensor_reading_repository.insert_sensor_reading(reading)
+        time.sleep(25)
 
 if __name__ == "__main__":
     main()
