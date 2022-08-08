45c3dc5e6b75bdb25bc7989fa473c61ef8e0ce84
Olli Ulmala <ulmala.olli@hotmail.com>
2022-08-08 20:40:41 +0300

    add function for getting all sensors from db

--- src/services/sensor_service.py
+++ src/services/sensor_service.py
@@ -6,7 +6,14 @@ from repositories.sensor_repository import (
 
 class SensorService:
     def __init__(self, sensor_repository=default_sensor_repository):
-        self._sensor_repository = default_sensor_repository
+        self._sensor_repository = sensor_repository
 
     def add_sensor(self, mac, name):
         self._sensor_repository.add(Sensor(mac, name))
+
+    def all_sensors(self):
+        sensors = self._sensor_repository.get_all_sensors()
+        return sensors
+
+
+sensor_service = SensorService()
