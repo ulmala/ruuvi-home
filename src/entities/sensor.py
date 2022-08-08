af8d31f37f7e207605298b8e31442b592e430f9b
Olli Ulmala <ulmala.olli@hotmail.com>
2022-08-08 20:01:13 +0300

    add full mac field

--- src/entities/sensor.py
+++ src/entities/sensor.py
@@ -1,6 +1,10 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 @dataclass
 class Sensor:
     mac: str
     name: str
+    full_mac: str = field(init=False)
+
+    def __post_init__(self):
+        self.full_mac = ':'.join(self.mac[i : i + 2] for i in range(0,12,2)).upper()
