1305e3ec412b065f3e6b84373a3e7d04192284ac
Olli Ulmala <ulmala.olli@hotmail.com>
2022-08-08 20:40:08 +0300

    add script for adding sensors

---
+++ src/add_sensors.py
@@ -0,0 +1,4 @@
+from services.sensor_service import sensor_service
+
+sensor_service.add_sensor(mac='d3fdf83e3367', name='bedroom')
+sensor_service.add_sensor(mac='d92e2aee6843', name='living room')
